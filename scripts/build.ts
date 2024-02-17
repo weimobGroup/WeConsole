5a5264bd555f54ab72c39c594d80cf11fd96bef7
imingyu <mingyuhisoft@163.com>
2024-02-18 02:05:38 +0800

    build: 优化产物js数量和存放位置

--- scripts/build.ts
+++ scripts/build.ts
@@ -67,9 +67,15 @@ const getBuildOptions = (mode: 'full' | 'npm'): [RollupOptions, () => void] => {
         'main/init': ROOT_DIR + '/src/main/init.ts',
         ...components
     };
+    if (mode === 'full') {
+        input['subpackage/components/dynamic/index'] =
+            ROOT_DIR + '/node_modules/@cross-virtual-list/mp-wx/dist/npm/components/dynamic/index.js';
+        input['subpackage/components/regular/index'] =
+            ROOT_DIR + '/node_modules/@cross-virtual-list/mp-wx/dist/npm/components/regular/index.js';
+    }
     return [
         {
-            external: mode === 'full' ? [] : [/mpkit/, /typescript-mp-component/],
+            external: mode === 'full' ? [] : [/mpkit/, /typescript-mp-component/, /@@cross-virtual-list/],
             plugins: getPlugins(),
             input,
             output: [
@@ -79,20 +85,18 @@ const getBuildOptions = (mode: 'full' | 'npm'): [RollupOptions, () => void] => {
                     chunkFileNames: '[name].js',
                     hoistTransitiveImports: false,
                     manualChunks: (id) => {
-                        if (
-                            id.includes('main') &&
-                            !id.includes('main/index') &&
-                            !id.includes('main/init') &&
-                            !id.includes('main/demo')
-                        ) {
-                            return 'main/common';
-                        }
-                        if (id.includes('subpackage') && !id.includes('components')) {
-                            return 'main/common';
-                        }
-                        if (id.includes('node_modules')) {
+                        if (id.includes('node_modules') && !id.includes('components')) {
+                            if (id.includes('typescript-mp-component') || id.includes('@cross-virtual-list')) {
+                                return 'subpackage/vender';
+                            }
                             const name = 'main/vender';
                             return name;
+                        }
+                        if (id.includes('main') && !id.includes('main/index') && !id.includes('main/init')) {
+                            return 'main/common';
+                        }
+                        if (!id.includes('components') && !id.includes('node_modules')) {
+                            return id.includes('subpackage') ? 'subpackage/common' : 'main/common';
                         }
                     }
                 }
@@ -129,11 +133,30 @@ const getBuildOptions = (mode: 'full' | 'npm'): [RollupOptions, () => void] => {
                     }
                     return sum;
                 }, [])
-            ]).then(() => {
-                if (mode === 'full') {
-                    return copyPromise(ROOT_DIR + '/dist/' + mode + '/**/*', ROOT_DIR + '/examples/weconsole');
-                }
-            });
+            ])
+                .then(() => {
+                    if (mode === 'full') {
+                        return Promise.all([
+                            copyPromise(
+                                ROOT_DIR + '/node_modules/@cross-virtual-list/mp-wx/dist/npm/components/**/*.wxml',
+                                ROOT_DIR + '/dist/' + mode + '/subpackage/components'
+                            ),
+                            copyPromise(
+                                ROOT_DIR + '/node_modules/@cross-virtual-list/mp-wx/dist/npm/components/**/*.wxss',
+                                ROOT_DIR + '/dist/' + mode + '/subpackage/components'
+                            ),
+                            copyPromise(
+                                ROOT_DIR + '/node_modules/@cross-virtual-list/mp-wx/dist/npm/components/**/*.json',
+                                ROOT_DIR + '/dist/' + mode + '/subpackage/components'
+                            )
+                        ]);
+                    }
+                })
+                .then(() => {
+                    if (mode === 'full') {
+                        return copyPromise(ROOT_DIR + '/dist/' + mode + '/**/*', ROOT_DIR + '/examples/weconsole');
+                    }
+                });
         }
     ];
 };
