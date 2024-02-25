4d298ec040cbb1566168cd7e5c49a0d48a14c88f
imingyu <mingyuhisoft@163.com>
2024-02-25 14:11:35 +0800

    fix: registerClassComponent bug

--- src/subpackage/mixins/component.ts
+++ src/subpackage/mixins/component.ts
@@ -2,7 +2,7 @@ import type { MpComponent } from 'typescript-mp-component';
 import { toMpComponentConfig } from 'typescript-mp-component';
 
 export const registerClassComponent = (constructor: new () => MpComponent) => {
-    registerClassComponent(toMpComponentConfig(constructor));
+    registerComponent(toMpComponentConfig(constructor));
 };
 
 export const registerComponent = (spec: any) => {
